6c6281ff5201ff61788df1b6acee415f46da9476
Ratnakar Pedagani <rpedagani@schoolsfirstfcu.org>
2019-10-30 13:12:07 -0700

    Adding CheckPermutation Project

---
+++ CheckPermutation/CheckPermutation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "#1";
+            string s2 = "#84";
+        }
+
+        string[] permutations(string s)
+        {
+            var permutationString = new string[s.Length];
+            var count = 0;
+            for(int i=0; i< s.Length; i++)
+            {
+                permutationString[count] += s[i];
+                count++;
+            }
+
+            return permutationString;
+        }
+    }
+}
